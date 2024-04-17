1a77f5e472afe5ebc46b8e0b48a315253debf97d
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-17 16:45:20 +0200

    commenti e abbellimenti

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -9,6 +9,7 @@ namespace csharp_typesystem_snacks
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("SNACK 1");
             //Snack 1 L’utente inserisce due numeri in successione. Il software stampa il maggiore.
 
             //prendo primo numero
@@ -28,8 +29,8 @@ namespace csharp_typesystem_snacks
             {
                 Console.WriteLine($"Il numero più grande fra i due è: {Math.Max(num1, num2)}");
             }
-
             //Snack 2 L’utente inserisce due parole in successione. Il software stampa prima la parola più corta, poi la parola più lunga.
+            Console.WriteLine("SNACK 2");
 
             //prendo prima parola
             Console.WriteLine("Inserisci una frase o parola");
@@ -50,6 +51,7 @@ namespace csharp_typesystem_snacks
             }
 
             // Snack 3 Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+            Console.WriteLine("SNACK 3");
             // inizializzo a 0 la somma dei numeri
             int sum = 0;
             //ciclo per 10 volte la richesta di inserimento di una cifra
@@ -66,6 +68,7 @@ namespace csharp_typesystem_snacks
 
 
             //Snack 4 Calcola la somma e la media dei numeri da 2 a 10
+            Console.WriteLine("SNACK 4");
             //inizializzo array di numeri
             int[] numeri = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             //inizializzo la somma a 0
@@ -81,6 +84,7 @@ namespace csharp_typesystem_snacks
             Console.WriteLine($"La media dei numeri è: {somma / numeri.Length}");
 
             //Snack 5 Il software chiede all’utente di inserire un numero.Se il numero inserito è pari, stampa il numero, se è dispari, stampa il numero successivo.
+            Console.WriteLine("SNACK 5");
             //chiedo all'user il numero
             Console.WriteLine("Inserisci un numero casuale");
             int userNum = Convert.ToInt32(Console.ReadLine());
@@ -97,7 +101,7 @@ namespace csharp_typesystem_snacks
             }
 
             //Snack 6 In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby. Chiedi all’utente il suo nome e comunicagli se può partecipare o meno alla festa.
-
+            Console.WriteLine("SNACK 6");
             string[] gatsbyPartyGuest = { "Jack", "John", "Mary", "Marilyn", "George", "Christopher" };
 
             Console.WriteLine("Inserisci il tuo nome e ti dirò se sei invitato alla festa...");
@@ -111,6 +115,7 @@ namespace csharp_typesystem_snacks
             }
 
             //Snack 7 Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
+            Console.WriteLine("SNACK 7");
             //creo array vuoto
             int[] arrDispari = new int[6];
             //sett il suo indice a 0
@@ -138,6 +143,7 @@ namespace csharp_typesystem_snacks
             }
 
             // Snack 8 Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
+            Console.WriteLine("SNACK 8");
             //creo array
             int[] numbers = { 1, 9, 2, 8, 3, 7, 4, 5, 6 };
             //setto la somma a 0
@@ -151,9 +157,10 @@ namespace csharp_typesystem_snacks
                     add += numbers[i];
                 }
             }
-            Console.WriteLine($"La somma dei numeri dispari inclusi nell'array è: {add}");
+            Console.WriteLine($"La somma dei numeri con indice dispari inclusi nell'array è: {add}");
 
             // Snack 9 Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.Continua a chiedere i numeri all’utente e a inserirli nell’array, fino a quando la somma degli elementi è minore di 50.
+            Console.WriteLine("SNACK 9");
             //creo arr vuoto
             int[] arrVuoto = new int[20];
             //setto il suo indice a 0
@@ -181,6 +188,7 @@ namespace csharp_typesystem_snacks
             }
 
             //Snack 10:  Fai inserire un numero, che chiameremo N, all’utente.
+            Console.WriteLine("SNACK 10");
             //Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
             //Ogni volta che ne crei uno, stampalo a schermo.
 
